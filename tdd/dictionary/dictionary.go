dfe61f24a9486cd4943362fd4deaedc6aff8834d
Ivan Arsenev <git@ancill.com>
2024-08-28 22:36:08 -0300

    dictionary

--- tdd/dictionary/dictionary.go
+++ tdd/dictionary/dictionary.go
@@ -4,7 +4,16 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -15,6 +24,39 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) {
 	d[word] = definition
 }
+
+// So when you pass a map to a function/method, you are indeed copying it, but just the pointer part, not the underlying data structure that contains the data.
+
+// A gotcha with maps is that they can be a nil value. A nil map behaves like an empty map when reading, but attempts to write to a nil map will cause a runtime panic. You can read more about maps here.
+
+// Therefore, you should never initialize a nil map variable:
+
+// var m map[string]string
+
+// Instead, you can initialize an empty map or use the make keyword to create a map for you:
+
+// var dictionary = map[string]string{}
+
+// // OR
+
+// var dictionary = make(map[string]string)
+
+// Both approaches create an empty hash map and point dictionary at it. Which ensures that you will never get a runtime panic.
