6c8dd2547d316878d09dba0361a12e8513df9314
Ivan Arsenev <git@ancill.com>
2024-08-28 22:17:51 -0300

    1

--- tdd/dictionary/dictionary_test.go
+++ tdd/dictionary/dictionary_test.go
@@ -3,24 +3,34 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := Dictionary{"test": "this is just a test"}
+	d := Dictionary{"test": "this is just a test"}
 	t.Run("known word", func(t *testing.T) {
-		got, _ := dictionary.Search("test")
+		got, _ := d.Search("test")
 		want := "this is just a test"
 
 		assertStrings(t, got, want)
 	})
 
 	t.Run("unknown word", func(t *testing.T) {
-		_, err := dictionary.Search("unknown")
-		want := "could not find the word you were looking for"
-
-		if err == nil {
+		_, got := d.Search("unknown")
+		if got == nil {
 			t.Fatal("expected to get an error.")
 		}
-
-		assertStrings(t, err.Error(), want)
+		assertError(t, got, ErrNotFound)
 	})
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	assertStrings(t, got, want)
 }
 
 func assertStrings(t testing.TB, got, want string) {
@@ -28,5 +38,13 @@ func assertStrings(t testing.TB, got, want string) {
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
