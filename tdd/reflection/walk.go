3f8c115ca2acf703136c64035cd6130d1bfccb73
Ivan Arsenev <iv.arsenev@gmail.com>
2024-09-03 18:52:05 -0300

    sync started

--- tdd/reflection/walk.go
+++ tdd/reflection/walk.go
@@ -2,17 +2,66 @@ package walk
 
 import "reflect"
 
+// func walk(x interface{}, fn func(input string)) {
+// 	val := getValue(x)
+
+//		switch val.Kind() {
+//		case reflect.Struct:
+//			for i := 0; i < val.NumField(); i++ {
+//				walk(val.Field(i).Interface(), fn)
+//			}
+//		case reflect.Slice:
+//			for i := 0; i < val.Len(); i++ {
+//				walk(val.Index(i).Interface(), fn)
+//			}
+//		case reflect.String:
+//			fn(val.String())
+//		}
+//	}
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
+	val := getValue(x)
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-		if field.Kind() == reflect.String {
-			fn(field.String())
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
 		}
-
-		if field.Kind() == reflect.Struct {
-			walk(field.Interface(), fn)
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.Chan:
+		for {
+			if v, ok := val.Recv(); ok {
+				walkValue(v)
+			} else {
+				break
+			}
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walkValue(res)
 		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return val
 }
