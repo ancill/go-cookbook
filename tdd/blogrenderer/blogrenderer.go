58eb1e699ebcb6a17f18eefdd0c08055abe46511
Ivan Arsenev <iv.arsenev@gmail.com>
2024-09-11 15:57:06 -0300

    templates

--- tdd/blogrenderer/blogrenderer.go
+++ tdd/blogrenderer/blogrenderer.go
@@ -1,14 +1,28 @@
 package blogrenderer
 
 import (
-	"fmt"
+	"embed"
+	"html/template"
 	"io"
 
 	blogposts "github.com/ancill/go-cookbook/blogrenderer/blogpost"
 )
 
-func Render(w io.Writer, p blogposts.Post) error {
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
 
-	_, err := fmt.Fprintf(w, "<h1>%s</h1>", p.Title)
-	return err
+// templ, err := template.New("blog").Parse(postTemplate)
+func Render(w io.Writer, p blogposts.Post) error {
+	templ, err := template.ParseFS(postTemplates, "templates/*.html")
+	if err != nil {
+		return err
+	}
+
+	if err := templ.Execute(w, p); err != nil {
+		return err
+	}
+
+	return nil
 }
