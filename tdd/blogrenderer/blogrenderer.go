a0cd454db3a625449c972792bcfac503dd70847e
Ivan Arsenev <iv.arsenev@gmail.com>
2024-09-13 18:23:41 -0300

    1

--- tdd/blogrenderer/blogrenderer.go
+++ tdd/blogrenderer/blogrenderer.go
@@ -2,10 +2,9 @@ package blogrenderer
 
 import (
 	"embed"
+	blogposts "github.com/ancill/go-cookbook/blogrenderer/blogpost"
 	"html/template"
 	"io"
-
-	blogposts "github.com/ancill/go-cookbook/blogrenderer/blogpost"
 )
 
 var (
@@ -13,14 +12,21 @@ var (
 	postTemplates embed.FS
 )
 
-// templ, err := template.New("blog").Parse(postTemplate)
-func Render(w io.Writer, p blogposts.Post) error {
-	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.html")
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	if err := templ.Execute(w, p); err != nil {
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p blogposts.Post) error {
+	if err := r.templ.ExecuteTemplate(w, "blog.html", p); err != nil {
 		return err
 	}
 
--- tdd/blogrenderer/blogrenderer_test.go
+++ tdd/blogrenderer/blogrenderer_test.go
@@ -2,39 +2,13 @@ package blogrenderer_test
 
 import (
 	"bytes"
+	blogposts "github.com/ancill/go-cookbook/blogrenderer/blogpost"
+	approvals "github.com/approvals/go-approval-tests"
+	"io"
 	"testing"
 
 	"github.com/ancill/go-cookbook/blogrenderer"
-	blogposts "github.com/ancill/go-cookbook/blogrenderer/blogpost"
-	approvals "github.com/approvals/go-approval-tests"
 )
-
-// func TestRender(t *testing.T) {
-// 	var (
-// 		aPost = blogposts.Post{
-// 			Title:       "hello world",
-// 			Body:        "This is a post",
-// 			Description: "This is a description",
-// 			Tags:        []string{"go", "tdd"},
-// 		}
-// 	)
-
-// 	t.Run("it converts a single post into HTML", func(t *testing.T) {
-// 		buf := bytes.Buffer{}
-// 		err := blogrenderer.Render(&buf, aPost)
-
-// 		if err != nil {
-// 			t.Fatal(err)
-// 		}
-
-// 		got := buf.String()
-// 		want := `<h1>hello world</h1><p>This is a description</p>Tags: <ul><li>go</li><li>tdd</li></ul>`
-
-// 		if got != want {
-// 			t.Errorf("got '%s' want '%s'", got, want)
-// 		}
-// 	})
-// }
 
 func TestRender(t *testing.T) {
 	var (
@@ -46,13 +20,40 @@ func TestRender(t *testing.T) {
 		}
 	)
 
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
 
-		if err := blogrenderer.Render(&buf, aPost); err != nil {
+		if err := postRenderer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
-
 		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	var (
+		aPost = blogposts.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		postRenderer.Render(io.Discard, aPost)
+	}
 }
